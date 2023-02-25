2b601b830d1f63872b9c97619603653559fd5ddc
Chandan Rajah <chandan.rajah@gmail.com>
2023-02-25 15:27:15 +0000

    Licence API Key set as Kubernetes secret and used when making a call to Licence service

--- src/main/scala/parallelai/sot/api/config/Api.scala
+++ src/main/scala/parallelai/sot/api/config/Api.scala
@@ -2,6 +2,6 @@ package parallelai.sot.api.config
 
 import java.net.URI
 
-case class Api(name: String, context: String, version: String, uri: URI, ssl: Boolean)
+case class Api(name: String, context: String, version: String, uri: URI, ssl: Boolean = false)
 
-case class Licence(name: String, context: String, version: String, uri: URI, port: Int, ssl: Boolean)
+case class Licence(name: String, context: String, version: String, uri: URI, port: Int, ssl: Boolean = false, apiKey: Option[String] = None)
--- src/main/scala/parallelai/sot/api/mechanics/GoogleCloudClient.scala
+++ src/main/scala/parallelai/sot/api/mechanics/GoogleCloudClient.scala
@@ -18,16 +18,17 @@ object GoogleJobStatus {
 
 trait GoogleCloudClient {
   private val httpTransport = GoogleNetHttpTransport.newTrustedTransport
-  private val jsonFactory = JacksonFactory.getDefaultInstance
-  val dataflow: Dataflow = new Dataflow.Builder(httpTransport, jsonFactory, credential).setApplicationName(api.name).build
 
-  private def credential: GoogleCredential = {
-    // Authentication is provided by gcloud tool when running locally// Authentication is provided by gcloud tool when running locally
-    // and by built-in service accounts when running on GAE, GCE or GKE.
+  private val jsonFactory = JacksonFactory.getDefaultInstance
+
+  protected lazy val dataflow: Dataflow = new Dataflow.Builder(httpTransport, jsonFactory, credential).setApplicationName(api.name).build
+
+  protected lazy val credential: GoogleCredential = {
+    // Authentication is provided by gcloud tool when running locally and by built-in service accounts when running on GAE, GCE or GKE.
     val credential = GoogleCredential.getApplicationDefault
 
-    // The createScopedRequired method returns true when running on GAE or a local developer
-    // machine. In that case, the desired scopes must be passed in manually. When the code is
+    // The createScopedRequired method returns true when running on GAE or a local developer machine.
+    // In that case, the desired scopes must be passed in manually. When the code is
     // running in GCE, GKE or a Managed VM, the scopes are pulled from the GCE metadata server.
     // See https://developers.google.com/identity/protocols/application-default-credentials for more information.
     if (credential.createScopedRequired) {
