f8c021ae1373d89f925c2ce124e97981ff48896d
Chandan Rajah <chandan.rajah@gmail.com>
2023-03-05 19:40:59 +0000

    Combining Spray JSON and Circe with Sttp

--- src/main/scala/parallelai/sot/api/endpoints/ProductEndpoints.scala
+++ src/main/scala/parallelai/sot/api/endpoints/ProductEndpoints.scala
@@ -2,6 +2,12 @@ package parallelai.sot.api.endpoints
 
 import scala.concurrent.Future
 import grizzled.slf4j.Logging
+import io.circe.Decoder.Result
+import io.circe.syntax._
+import io.circe._
+import io.circe.parser._
+import cats.syntax.either._
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import io.finch._
 import io.finch.sprayjson._
 import io.finch.syntax._
@@ -11,6 +17,7 @@ import spray.json._
 import spray.json.lenses.JsonLenses._
 import com.softwaremill.sttp._
 import com.softwaremill.sttp.circe._
+import com.twitter.finagle.http.Status
 import parallelai.common.secure.diffiehellman.{ClientPublicKey, ClientSharedSecret, DiffieHellmanClient, ServerPublicKey}
 import parallelai.common.secure.{AES, CryptoMechanic}
 import parallelai.sot.api.concurrent.WebServiceExecutionContext
@@ -18,6 +25,11 @@ import parallelai.sot.api.config._
 import parallelai.sot.api.model.Product
 
 trait ProductEndpoints extends EndpointOps with LicenceEndpointOps with DefaultJsonProtocol with Logging {
+
+
+
+
+
   implicit val crypto: CryptoMechanic = new CryptoMechanic(AES, secret = secret.getBytes)
 
   val productPath: Endpoint[HNil] = api.path :: "product"
@@ -33,14 +45,56 @@ trait ProductEndpoints extends EndpointOps with LicenceEndpointOps with DefaultJ
       val request: Request[String, Nothing] =
         sttp post licenceUri"/product/register" body productRegister
 
-      request.send.map { response =>
+      ///////////////////////////////////
+
+      implicit val statusEncoder: Encoder[Status] = (status: Status) =>
+        Json.obj(
+          ("code", Json.fromInt(status.code)),
+          ("reason", Json.fromString(status.reason))
+        )
+
+      implicit val statusDecoder: Decoder[Status] = (hcursor: HCursor) => for {
+        code <- hcursor.downField("code").as[Int]
+      } yield Status(code)
+
+      implicit val responseEncoder: Encoder[Response] = (response: Response) =>
+        Json.obj(
+          ("content", parse(response.content.compactPrint).right.get),
+          ("status", response.status.asJson)
+        )
+
+      implicit val responseDecoder: Decoder[Response] = new Decoder[Response] {
+        def apply(c: HCursor): Result[Response] = {
+          import Response._
+
+          Right(Response(c.downField("content").focus.get.noSpaces.parseJson, c.downField("status").focus.get.noSpaces.parseJson.convertTo[Status]))
+        }
+      }
+
+      /*(hcursor: HCursor) => for {
+        //content <- hcursor.downField("content").focus.get.noSpaces.parseJson
+        //status <- hcursor.downField("status").as[Status]
+      } yield Response(hcursor.downField("content").focus.get.noSpaces.parseJson, hcursor.downField("status").focus.get.noSpaces.parseJson.convertTo[Status])*/
+
+
+
+      ///////////////////////////////////
+
+      /*
+      def asJson[B: Decoder]: ResponseAs[Either[io.circe.Error, B], Nothing] =
+    asString(Utf8).map(decode[B])
+       */
+
+      def asSprayJson[B: JsonReader]: ResponseAs[B, Nothing] = asString("utf-8").map(s => s.parseJson.convertTo[B])
+
+      request.response(asSprayJson[Response]).send.map { response =>
         val rep = response.body
         println(s"===> RRRRR $rep")
 
         response.body match {
           case Right(r) =>
             println(s"===> THE r IS: $r")
-            val serverPublicKey = r.parseJson.extract[ServerPublicKey]("content")
+            val serverPublicKey = r.content.convertTo[ServerPublicKey]
             val clientSharedSecret: ClientSharedSecret = DiffieHellmanClient.createClientSharedSecret(serverPublicKey)
             println(s"===> x = $clientSharedSecret")
 
