14626f5bea003d7e3ba71b8b09377f6d60d51d7f
Chandan Rajah <chandan.rajah@gmail.com>
2023-03-07 17:55:24 +0000

    Ready for "register product" endpoint to use "shared" service which has a contract for multiple microservices to abide by - in our case the microservices for API and Licence

--- src/test/scala/parallelai/sot/api/http/service/RegisterProductImplSpec.scala
+++ src/test/scala/parallelai/sot/api/http/service/RegisterProductImplSpec.scala
@@ -2,26 +2,40 @@ package parallelai.sot.api.http.service
 
 import scala.concurrent.Future
 import cats.Id
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{MustMatchers, WordSpec}
 import com.softwaremill.sttp.testing.SttpBackendStub
 import com.twitter.finagle.http.Status
-import parallelai.common.secure.diffiehellman.{DiffieHellmanClient, DiffieHellmanServer}
+import parallelai.common.secure.diffiehellman.{ClientPublicKey, DiffieHellmanClient, DiffieHellmanServer}
 import parallelai.common.secure.{AES, Crypto, CryptoMechanic, Encrypted}
 import parallelai.sot.api.config.secret
 import parallelai.sot.api.http.{Errors, Result}
 import parallelai.sot.api.model._
 
-class RegisterProductImplSpec extends WordSpec with MustMatchers {
+class RegisterProductImplSpec extends WordSpec with MustMatchers with ScalaFutures with IdGenerator99UniqueSuffix {
   implicit val crypto: CryptoMechanic = new CryptoMechanic(AES, secret = secret.getBytes)
 
-  "Registration of a product" should {
-    "" in {
-      val (serverPublicKey, serverSharedSecret) = DiffieHellmanServer.create(DiffieHellmanClient.createClientPublicKey)
-      val registeredProduct = RegisteredProduct(serverPublicKey, Encrypted(ApiSharedSecret(IdGenerator.uniqueId(), Crypto.aesSecretKey)))
+  val clientPublicKey: ClientPublicKey = DiffieHellmanClient.createClientPublicKey
 
+  val (serverPublicKey, serverSharedSecret) = DiffieHellmanServer.create(clientPublicKey)
+
+  val registeredProduct = RegisteredProduct(serverPublicKey, Encrypted(ApiSharedSecret(uniqueId(), Crypto.aesSecretKey)))
+
+  class RegisterProductSimpleImpl extends RegisterProduct[Id] {
+    def apply(product: Product): Id[Result[RegisteredProduct]] =
+      Result(Right(registeredProduct), Status.Ok)
+  }
+
+  class RegisterProductSimpleErrorImpl extends RegisterProduct[Id] {
+    def apply(product: Product): Id[Result[RegisteredProduct]] =
+      Result(Left(Errors("Whoops")), Status.BadRequest)
+  }
+
+  "Registration of a product" should {
+    "be successful" in {
       implicit val backend: SttpBackendStub[Future, Nothing] = {
         SttpBackendStub.asynchronousFuture
-          .whenRequestMatches(req => true)
+          .whenRequestMatches(_ => true)
           .thenRespond(Result(registeredProduct, Status.Ok))
       }
 
@@ -30,24 +44,38 @@ class RegisterProductImplSpec extends WordSpec with MustMatchers {
       val productToken = ProductToken("licenceId", "productCode", "productEmail")
       val product = Product(productToken.code, productToken.email, Encrypted(productToken))
 
-      registerProduct(product)
-    }
+      val result: Future[Result[RegisteredProduct]] = registerProduct(product)
 
-    "x" in {
-      val registerProduct = new RegisterProductImpl2
+      whenReady(result) { r =>
+        r.status mustEqual Status.Ok
+        r.value.right.get mustEqual registeredProduct
+      }
+    }
+  }
+
+  "Simple registration of a product" should {
+    "be successful" in {
+      val registerProduct = new RegisterProductSimpleImpl
 
       val productToken = ProductToken("licenceId", "productCode", "productEmail")
       val product = Product(productToken.code, productToken.email, Encrypted(productToken))
 
-      registerProduct(product)
+      val result: Id[Result[RegisteredProduct]] = registerProduct(product)
+
+      result.status mustEqual Status.Ok
+      result.value mustBe a [Right[_, _]]
+    }
+
+    "fail" in {
+      val registerProduct = new RegisterProductSimpleErrorImpl
+
+      val productToken = ProductToken("licenceId", "productCode", "productEmail")
+      val product = Product(productToken.code, productToken.email, Encrypted(productToken))
+
+      val result: Id[Result[RegisteredProduct]] = registerProduct(product)
+
+      result.status mustEqual Status.BadRequest
+      result.value mustBe Left(Errors("Whoops"))
     }
   }
-}
-
-class RegisterProductImpl2 extends RegisterProduct[Id] {
-  def apply(product: Product): Id[Result[RegisteredProduct]] = {
-    println("Nice - got errors")
-    Result(Left(Errors("blah")), Status.BadRequest)
-  }
-
 }
