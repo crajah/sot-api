837706771c9a76bcbc3799d653cb5ba4cc1199aa
Chandan Rajah <chandan.rajah@gmail.com>
2023-03-11 16:07:24 +0000

    Health endpoints using new Result (instead of Response)

--- src/main/scala/parallelai/sot/api/http/endpoints/HealthEndpoints.scala
+++ src/main/scala/parallelai/sot/api/http/endpoints/HealthEndpoints.scala
@@ -2,31 +2,38 @@ package parallelai.sot.api.http.endpoints
 
 import scala.concurrent.Future
 import grizzled.slf4j.Logging
-import io.finch._
 import io.finch.syntax._
+import io.finch.{Errors => _, _}
 import shapeless.HNil
-import spray.json.{DefaultJsonProtocol, _}
 import com.softwaremill.sttp._
+import com.twitter.finagle.http.Status
 import parallelai.sot.api.concurrent.WebServiceExecutionContext
 import parallelai.sot.api.config._
+import parallelai.sot.api.http.{Errors, Result, ResultOps}
 
-class HealthEndpoints(implicit sb: SttpBackend[Future, Nothing]) extends EndpointOps with LicenceEndpointOps with DefaultJsonProtocol with Logging {
+class HealthEndpoints(implicit sb: SttpBackend[Future, Nothing]) extends EndpointOps with LicenceEndpointOps with ResultOps with Logging {
   val healthPath: Endpoint[HNil] = api.path :: "health"
 
   lazy val healthEndpoints = licenceHealth :+: health
 
-  protected lazy val licenceHealth: Endpoint[Response] = {
+  protected lazy val licenceHealth: Endpoint[Result[String]] = {
     implicit val ec: WebServiceExecutionContext = WebServiceExecutionContext()
 
     get(healthPath :: licence.context) {
-      val request: Request[String, Nothing] = sttp get licenceUri"/health"
-      request.send.map(r => Response(r.unsafeBody.parseJson)).toTFuture
+      val request: Request[Result[String], Nothing] = sttp get licenceUri"/health" response asJson[Result[String]]
+
+      request.send.map { response =>
+        response.body match {
+          case Right(result) => result
+          case Left(error) => Result[String](Left(Errors(error)), Status.UnprocessableEntity)
+        }
+      } toTFuture
     }
   }
 
-  protected lazy val health: Endpoint[Response] =
+  protected lazy val health: Endpoint[Result[String]] =
     get(healthPath) {
-      Response(s"Successfully pinged service ${api.name}").toTFuture
+      Result(s"Successfully pinged service ${api.name}", Status.Ok).toTFuture
     }
 }
 
--- src/test/scala/parallelai/sot/api/http/endpoints/HealthEndpointsSpec.scala
+++ src/test/scala/parallelai/sot/api/http/endpoints/HealthEndpointsSpec.scala
@@ -2,38 +2,37 @@ package parallelai.sot.api.http.endpoints
 
 import scala.concurrent.Future
 import io.finch.Input._
-import spray.json._
-import spray.json.lenses.JsonLenses._
 import org.scalatest.{MustMatchers, WordSpec}
+import com.softwaremill.sttp.SttpBackend
 import com.softwaremill.sttp.okhttp.OkHttpFutureBackend
 import com.softwaremill.sttp.testing.SttpBackendStub
-import com.softwaremill.sttp.{SttpBackend, Response => _}
 import com.twitter.finagle.http.Status
 import parallelai.sot.api.config._
+import parallelai.sot.api.http.Result
 
-class HealthEndpointsSpec extends WordSpec with MustMatchers with ResponseOps {
+class HealthEndpointsSpec extends WordSpec with MustMatchers {
   "Health endpoints" should {
     "indicate a healthy service" in {
       implicit val okSttpFutureBackend: SttpBackend[Future, Nothing] = OkHttpFutureBackend()
 
       new HealthEndpoints {
-        val Some(response) = health(get(p"/$healthPath")).awaitValueUnsafe()
+        val Some(result) = health(get(p"/$healthPath")).awaitValueUnsafe()
 
-        response.status mustEqual Status.Ok
-        response.content.convertTo[String] mustEqual s"Successfully pinged service ${api.name}"
+        result.status mustEqual Status.Ok
+        result.value.right.get mustEqual s"Successfully pinged service ${api.name}"
       }
     }
 
     "indicate a healthy licence service" in {
       implicit val backend: SttpBackendStub[Future, Nothing] = SttpBackendStub.asynchronousFuture
         .whenRequestMatches(req => req.uri.host.contains(licence.name) && req.uri.path.startsWith(Seq(licence.context, licence.version, "health")))
-        .thenRespond(Response(s"Successfully pinged service ${licence.name}").toJson.prettyPrint)
+        .thenRespond(Result(s"Successfully pinged service ${licence.name}", Status.Ok))
 
       new HealthEndpoints {
-        val Some(response) = licenceHealth(get(p"/$healthPath/${licence.context}")).awaitValueUnsafe()
+        val Some(result) = licenceHealth(get(p"/$healthPath/${licence.context}")).awaitValueUnsafe()
 
-        response.status mustEqual Status.Ok
-        response.content.extract[String]("content") mustEqual s"Successfully pinged service ${licence.name}"
+        result.status mustEqual Status.Ok
+        result.value.right.get mustEqual s"Successfully pinged service ${licence.name}"
       }
     }
   }
