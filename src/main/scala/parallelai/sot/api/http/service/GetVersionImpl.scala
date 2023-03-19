72680d844418ca75c5968acb5607200fc35c87e6
Chandan Rajah <chandan.rajah@gmail.com>
2023-03-19 10:36:13 +0000

    Nice file names

--- src/main/scala/parallelai/sot/api/http/service/GetVersionImpl.scala
+++ src/main/scala/parallelai/sot/api/http/service/GetVersionImpl.scala
@@ -1,23 +1,24 @@
 package parallelai.sot.api.http.service
 
-import java.util.UUID
 import scala.concurrent.Future
 import better.files._
 import cats.implicits._
 import com.softwaremill.sttp._
 import parallelai.sot.api.concurrent.ExecutionContexts.webServiceExecutionContext
-import parallelai.sot.api.config.baseDirectory
+import parallelai.sot.api.config._
+import parallelai.sot.api.file.GCFileNameConverter._
 import parallelai.sot.api.http.ResultOps
 import parallelai.sot.api.http.endpoints.LicenceEndpointOps
 import parallelai.sot.api.model.RegisteredVersion
 
 class GetVersionImpl(implicit sb: SttpBackend[Future, Nothing]) extends GetVersion[Future] with LicenceEndpointOps with ResultOps {
   def apply(registeredVersion: RegisteredVersion): Future[String Either File] = {
-    val request =
-      sttp post licenceUri"/file" body registeredVersion response asFile((baseDirectory / s"${UUID.randomUUID().toString}.zip").createIfNotExists(createParents = true).toJava, overwrite = true)
-      // TODO - Sort out "random ID" !!! Unable to write to...
+    val file = (executor.directory / registeredVersion.defineFileName).createIfNotExists(createParents = true)
 
-    println(s"===> API: sending request to licence for $registeredVersion")
+    val request =
+      sttp post licenceUri"/file" body registeredVersion response asFile(file.toJava, overwrite = true)
+
+    println(s"===> API: sending request to licence for $registeredVersion to get associated file: $file") // TODO - Remove
     request.send.map(_.body.map(_.toScala))
   }
 }
